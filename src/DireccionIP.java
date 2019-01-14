9a9ef3be56ccd29b2014c1a1aeb863a3f4a366cf
Jhordan <Jhordan99@gmail.com>
2019-01-14 01:06:24 +0100

    @migueldavidorrico He creado una nueva clase, en la que defino la estructura de las IP(EstructuraIP) y luego desde la de DireccionIP llamo a los constructores con un metodo, la llamada a los constructores los he definido todos abajo ya. Ahora solo tengo que escribir lo de dentro de los constructores en Estructura IP. Tengo un par de dudas, que no se porque me sale error en Direccion IP, si lo pongo como tipo int y no como String se arregla pero si lo pongo como int no me deja luego utilizar el .split en la clase EstructuraIP(lo de .split lo he visto en internet, no se si se podrá utilizar o he de hacerlo de otra manera)

---
+++ src/DireccionIP.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import javax.swing.Icon;
+
+public class DireccionIP {
+    public static void main(String[] args) {
+        Icon miIcono = new ImageIcon("ip-address.png");
+        Object elecciones[] = {"Cadena Normal.", "En cuatro partes.", "Salir."};
+        int noTeSalgas=1;
+        while(noTeSalgas==1){
+            int eleccionPrincipalDelMetodo = JOptionPane.showOptionDialog(
+                null,
+                "Elige una manera de introducir las IP",
+                "Elige como introducir la IP",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                miIcono,
+                elecciones,
+                elecciones[2]);
+            if (eleccionPrincipalDelMetodo==0){
+                String salida = JOptionPane.showInputDialog(null, "Introduzca la IP separada por puntos");
+                EstructuraIP ip = new EstructuraIP(salida);
+                mostrarInformacionSobreLaIP(ip);
+
+        }  else if(eleccionPrincipalDelMetodo == 1){
+                int contadorBytes=1;
+                int[] bytes = new int[4];
+                String salida = "";
+                for (int i= 0; i<bytes.length; i++){
+                    salida = JOptionPane.showInputDialog(null, "Introduce el byte:"+contadorBytes);
+                    bytes[i] = Integer.parseInt(salida);
+                    contadorBytes++;
+                }
+                EstructuraIP ip = new EstructuraIP(bytes);
+                mostrarInformacionSobreLaIP(ip);
+
+            }else {
+                noTeSalgas=JOptionPane.showConfirmDialog(null,"¿De verdad quieres salir?","Salir",JOptionPane.YES_NO_CANCEL_OPTION);
+            }
+        }
+    }
+
+    public static void mostrarInformacionSobreLaIP(EstructuraIP ip) {
+        Icon miIcono = new ImageIcon("ip-address.png");
+            String salida = "";
+            salida += EstructuraIP.getDireccionIp() + "\n";
+            salida += EstructuraIP.getIdRed() + "\n";
+            salida += EstructuraIP.getMascaraRed() + "\n";
+            salida += EstructuraIP.getEsPrivada() + "\n";
+            salida += EstructuraIP.getClase() + "\n";
+            salida += EstructuraIP.getEsIdRed();
+
+            JOptionPane.showMessageDialog(null, salida, "INFORMACIÓN DE LA IP", 0, miIcono);
+        }
+    }
---
+++ src/EstructuraIP.java
@@ -0,0 +1,29 @@
+public class EstructuraIP {
+
+    public static String getDireccionIp() {
+    }
+
+    public static String getIdRed() {
+    }
+
+    public static String getMascaraRed() {
+    }
+
+    public static String getEsPrivada() {
+    }
+
+    public static String getClase() {
+    }
+
+    public static String getEsIdRed() {
+
+    }
+    private int[] bytes;
+    public EstructuraIP(String cadena) {
+        bytes = new int[4];
+        String[] partes = cadena.split("\\.");
+        for (int i=0; i<partes.length; i++){
+            bytes[i] = Integer.parseInt(partes[i]);
+        }
+    }
+}
